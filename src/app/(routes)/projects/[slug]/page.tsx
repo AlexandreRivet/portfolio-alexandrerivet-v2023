f46be5e1cfc8a52bb55437ad9fc729aaa3770292
AlexandreRivet <alex-rivet94@hotmail.fr>
2023-01-20 14:57:21 +0100

    Enhance navigation with data

--- src/app/(routes)/projects/[slug]/page.tsx
+++ src/app/(routes)/projects/[slug]/page.tsx
@@ -1,12 +1,16 @@
+import { notFound } from 'next/navigation';
+import { projects } from '@/data/projects';
+
 export default function Project({ params }: { params: { slug: string } }) {
-  return <div>Project { params.slug }</div>
+  const project = projects.find((p) => p.slug === params.slug);
+
+  if (!project) {
+    notFound();
+  }
+
+  return <div>Project: ({ project.title } / { project.period })</div>
 }
 
-export async function generateStaticParams() {
-  return [
-    { slug: 'georacing' },
-    { slug: 'dassault-systemes' },
-    { slug: 'wnp' },
-    { slug: 'playplay' },
-  ];
+export function generateStaticParams() {
+  return projects.map(({ slug }) => ({ slug }))
 }
