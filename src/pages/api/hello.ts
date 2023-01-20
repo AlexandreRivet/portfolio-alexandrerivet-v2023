d26d78db7a54bd78db25c01c72dd638046011676
AlexandreRivet <alex-rivet94@hotmail.fr>
2023-01-20 09:56:13 +0100

    Update api

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Alexandre Rivet' })
+  res.status(200).json({ name: 'RivAl' })
 }
