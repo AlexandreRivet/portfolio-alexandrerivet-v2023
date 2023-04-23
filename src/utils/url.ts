c40aded194d1d394995730b2ce5eea291bd1bff4
AlexandreRivet <alex-rivet94@hotmail.fr>
2023-04-23 12:33:36 +0200

    Fix mis function call

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,5 +3,5 @@ export const getRootUrl = () => `https://${process.env.NEXT_PUBLIC_URL}`;
 
 export const getPublicUrl = (subPart: string) => 
   process.env.VERCEL_ENV === 'production'
-    ? `${getRootUrl}${subPart}`
+    ? `${getRootUrl()}${subPart}`
     : process.env.VERCEL_URL;
